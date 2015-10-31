e683fd7037ee9a19c7f223b00756d71be6b2651b
hishamabdelhafez <hishamabdelhafez@gmail.com>
2015-10-31 18:56:48 +0200

    complete skeleton, home, blog, publications and resume complete

--- app/app.js
+++ app/app.js
@@ -13,6 +13,31 @@ var app = angular.module('PersonalApp', ['ngRoute']);
 //   $scope.hello = "HELLOW! Home";
 // });
 
+// app.directive('bsActiveLink', ['$location', function($location) {
+//   return {
+//     restrict: '*', //use as attribute
+//     replace: false,
+//     link: function(scope, elem) {
+//       //after the route has changed
+//       scope.$on("$routeChangeSuccess", function() {
+//         var hrefs = ['/#' + $location.path(),
+//           '#' + $location.path(), //html5: false
+//           $location.path()
+//         ]; //html5: true
+//         angular.forEach(elem.find('a'), function(a) {
+//           a = angular.element(a);
+//           if (-1 !== hrefs.indexOf(a.attr('href'))) {
+//             a.parent().addClass('active');
+//           } else {
+//             a.parent().removeClass('active');
+//           };
+//         });
+//       });
+//     }
+//   }
+// }]);
+//
+
 app.config([ '$routeProvider', function($routeProvider) {
   $routeProvider
    .when('/publications', {
@@ -21,8 +46,17 @@ app.config([ '$routeProvider', function($routeProvider) {
   .when('/home', {
     templateUrl: 'app/views/home.html'
   })
-  .when('/about', {
-    templateUrl: 'app/views/about.html'
+  .when('/contactme', {
+    templateUrl: 'app/views/contactme.html'
+    })
+    .when('/resume', {
+      templateUrl: 'app/views/resume.html'
+      })
+  .when('/bookshelf', {
+    templateUrl: 'app/views/bookshelf.html'
+    })
+  .when('/blog', {
+    templateUrl: 'app/views/blog.html'
     }).otherwise({
     redirectTo : '/home'
   });
